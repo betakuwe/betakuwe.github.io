2eb699b2aa2bb01b333a895c37f3e424d151dbd9
betakuwe <working.celery@gmail.com>
2024-05-20 19:14:09 +0800

    add favicon

--- src/core.clj
+++ src/core.clj
@@ -36,6 +36,31 @@
 (def resume
   (html
     [:head
+     ;; favicon
+     [:link
+      {:rel "apple-touch-icon",
+       :sizes "180x180",
+       :href "/favicon/apple-touch-icon.png"}]
+     [:link
+      {:rel "icon",
+       :type "image/png",
+       :sizes "32x32",
+       :href "/favicon/favicon-32x32.png"}]
+     [:link
+      {:rel "icon",
+       :type "image/png",
+       :sizes "16x16",
+       :href "/favicon/favicon-16x16.png"}]
+     [:link {:rel "manifest", :href "/favicon/site.webmanifest"}]
+     [:link
+      {:rel "mask-icon",
+       :href "/favicon/safari-pinned-tab.svg",
+       :color "#5bbad5"}]
+     [:link {:rel "shortcut icon", :href "/favicon/favicon.ico"}]
+     [:meta {:name "msapplication-TileColor", :content "#da532c"}]
+     [:meta
+      {:name "msapplication-config", :content "/favicon/browserconfig.xml"}]
+     [:meta {:name "theme-color", :content "#ffffff"}]
      ;; Fetch fonts
      [:link {:rel "preconnect" :href "https://fonts.googleapis.com"}]
      [:link {:rel         "preconnect"
@@ -158,7 +183,7 @@
       [:header.hide-in-print.box-shadow
        [:section
         [:div
-         [:img#display-profile {:src "images/display_profile.png"}]
+         [:img#display-profile {:src "images/display_profile.jpg"}]
          [:h1 {:style {:display "inline"}}
           "Benjamin Tan's résumé \uD83E\uDD17"]]
         [:button {:onclick "window.print()"}
@@ -292,8 +317,11 @@
 
 (defn get-assets []
   (assets/load-assets "public" ["/css/reset.css"
-                                #"/images/.*\.png$"
-                                #"/images/.*\.svg$"]))
+                                ;#"/images/.*\.png$"
+                                #"/images/.*\.svg$"
+                                #"/images/.*\.jpg$"
+                                #"/favicon/.*"
+                                ]))
 
 (def app
   (-> (stasis/serve-pages get-pages)
